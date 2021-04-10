dce7f2b25ffb9e073b9dd0f5115c7f462a2cc5ab
Franco <francoo.chen@gmail.com>
2021-04-09 22:54:10 -0400

    Working /globe endpoint with interactive globe

---
+++ frontend/src/Rooms.js
@@ -0,0 +1,24 @@
+import Globe from 'react-globe.gl'
+
+function handleClick() {
+    alert("u clicked a dot")
+}
+
+function Rooms() {
+    const N = 300;
+    const gData = [...Array(N).keys()].map(() => ({
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+        size: Math.random() / 3,
+        color: ['white'][Math.round(Math.random() * 3)]
+    }))
+
+    return (
+        <Globe globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+            pointsData={gData}
+            pointAltitude={0}
+            onPointClick={handleClick} />
+    )
+}
+
+export default Rooms
