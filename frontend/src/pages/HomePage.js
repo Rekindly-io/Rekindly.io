c4a9bfdbfc96146e0a3f95f3721cf3a86b57afec
codeislife1314 <athenaparthenos2@gmail.com>
2021-04-10 17:03:08 -0400

    globe

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,33 +1,48 @@
-import Globe from 'react-globe.gl'
-import AddRoomButton from '../Components/AddRoomButton';
-import './Room.css'; 
-function handleClick() {
-    alert("u clicked a dot")
-    
+import ReactGlobe from "react-globe";
+import AddRoomButton from "../Components/AddRoomButton";
+import "./Room.css";
+import markers from "../Components/globe/markers";
+import markerRenderer from "../Components/globe/markerRenderer";
+
+function handleClick(e) {
+  alert("u clicked a dot");
+  alert(e.target);
 }
 
 function HomePage() {
-    const N = 300;
-    const gData = [...Array(N).keys()].map(() => ({
-        lat: (Math.random() - 0.5) * 180,
-        lng: (Math.random() - 0.5) * 360,
-        size: Math.random() / 3,
-        color: ['white'][Math.round(Math.random() * 3)]
-    }))
+  const N = 300;
+  const gData = [...Array(N).keys()].map(() => ({
+    lat: (Math.random() - 0.5) * 180,
+    lng: (Math.random() - 0.5) * 360,
+    size: Math.random() / 3,
+    color: ["white"][Math.round(Math.random() * 3)],
+  }));
 
-    return (
-        <div>
-            <div>
-                <Globe globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                pointsData={gData}
-                pointAltitude={0}
-                onPointClick={handleClick} />
-            </div>
-            <div id="createRoom">
-                <AddRoomButton/> 
-            </div>
-        </div>
-    )
+  const options = {
+    markerRenderer,
+  };
+  /*<Globe
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+          pointsData={gData}
+          pointAltitude={0}
+          onPointClick={handleClick}
+
+        />*/
+  return (
+    <div>
+      <AddRoomButton />
+
+      <div>
+        <ReactGlobe
+          height="100vh"
+          globeTexture="https://raw.githubusercontent.com/chrisrzhou/react-globe/main/textures/globe_dark.jpg"
+          markers={markers}
+          width="100vw"
+          options={options}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- frontend/src/pages/RoomPage.js
+++ frontend/src/pages/RoomPage.js
@@ -4,7 +4,6 @@ import {useContext} from "react";
 
 
 function RoomPage() {
-
     const socket = useContext(SocketContext);
 
     return (
