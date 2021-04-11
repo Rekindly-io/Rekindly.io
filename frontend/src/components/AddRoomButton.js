caeb625f4364f8c8489df8fcd18457b889aca750
Gagan Bhat <gaganbhat6@gmail.com>
2021-04-11 14:37:56 -0500

    Make HTTPS friendly

--- frontend/src/components/AddRoomButton.js
+++ frontend/src/components/AddRoomButton.js
@@ -36,7 +36,7 @@ function AddRoomButton() {
     if (position.latitude) {
 
     } else {
-      fetch('http://geolocation-db.com/json/f9902210-97f0-11eb-a459-b997d30983f1')
+      fetch('https://geolocation-db.com/json/f9902210-97f0-11eb-a459-b997d30983f1')
       .then(res => res.json())
       .then((data) => {
         console.log(data)
